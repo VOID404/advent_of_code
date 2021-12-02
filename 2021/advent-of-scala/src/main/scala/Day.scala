45bd395b25b664c5e322989dbb76de38783e1360
void404 <wojciech.nawa@gmail.com>
2021-12-02 17:15:09 +0100

    Document `Day`

--- 2021/advent-of-scala/src/main/scala/Day.scala
+++ 2021/advent-of-scala/src/main/scala/Day.scala
@@ -1,7 +1,10 @@
 import scala.io.Source
 
 abstract class Day {
+  /** File with challange input */
   val inputFile: String
+
+  /** Returns the input file as iterable lines */
   def data =
     Source.fromFile(inputFile).getLines.toIterable
 }
