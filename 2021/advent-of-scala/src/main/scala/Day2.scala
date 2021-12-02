294b43a2d44f996436bd1d9be18da6f1bca1f59a
void404 <wojciech.nawa@gmail.com>
2021-12-02 15:10:00 +0100

    Solve day 2.2

--- 2021/advent-of-scala/src/main/scala/Day2.scala
+++ 2021/advent-of-scala/src/main/scala/Day2.scala
@@ -1,5 +1,6 @@
 import scala.io.Source
 import scala.runtime.stdLibPatches.language.unsafeNulls
+import scala.annotation.targetName
 
 object Day2 extends Day {
   override val inputFile = "inputs/day2.txt"
@@ -8,21 +9,40 @@ object Day2 extends Day {
   case class Command(val cmd: Cmd, val arg: Int) {
     def apply(pos: Position): Position =
       cmd match
-        case Cmd.Forward => Position(pos.x + arg, pos.y)
-        case Cmd.Up      => Position(pos.x, pos.y - arg)
-        case Cmd.Down    => Position(pos.x, pos.y + arg)
+        case Cmd.Forward => pos.copy(x = pos.x + arg)
+        case Cmd.Up      => pos.copy(y = pos.y - arg)
+        case Cmd.Down    => pos.copy(y = pos.y + arg)
+  }
+
+  case class Position2(val x: Int = 0, val y: Int = 0, val aim: Int = 0)
+  case class Command2(val cmd: Cmd, val arg: Int) {
+    def apply(pos: Position2): Position2 =
+      cmd match
+        case Cmd.Up   => pos.copy(aim = pos.aim - arg)
+        case Cmd.Down => pos.copy(aim = pos.aim + arg)
+        case Cmd.Forward =>
+          pos.copy(
+            x = pos.x + arg,
+            y = pos.y + pos.aim * arg
+          )
   }
   enum Cmd:
     case Forward, Down, Up
 
+  implicit def parseV2(cmd: Command): Command2 = Command2(cmd.cmd, cmd.arg)
+  implicit def parseV2(d: Iterable[Command]): Iterable[Command2] =
+    d.map(parseV2(_))
+
+  implicit def parse(cmd: String): Cmd =
+    cmd match
+      case "forward" => Cmd.Forward
+      case "up"      => Cmd.Up
+      case "down"    => Cmd.Down
+
   implicit def parse(d: Iterable[String]): Iterable[Command] =
     d.map { l =>
       val w = l.split(" ")
-      val cmd = w(0) match
-        case "forward" => Cmd.Forward
-        case "up"      => Cmd.Up
-        case "down"    => Cmd.Down
-        case _         => Cmd.Forward
+      val cmd = w(0)
 
       Command(cmd, w(1).toInt)
     }
@@ -32,5 +52,7 @@ object Day2 extends Day {
       .foldLeft(Position())((p, c) => c(p))
     pos.x * pos.y
 
-//   def partOne =
+  def partTwo(cmds: Iterable[Command] = data): Int =
+    val pos = cmds.foldLeft(Position2())((p, c) => c(p))
+    pos.x * pos.y
 }
--- 2021/advent-of-scala/src/test/scala/TestDay2.scala
+++ 2021/advent-of-scala/src/test/scala/TestDay2.scala
@@ -17,3 +17,19 @@ class TestDay2:
   @Test def partOne(): Unit =
     val res = Day2.partOne()
     assertEquals(res, 1561344)
+  
+  @Test def exampleV2(): Unit =
+    val cmds = List(
+      "forward 5",
+      "down 5",
+      "forward 8",
+      "up 3",
+      "down 8",
+      "forward 2"
+    )
+    val res = Day2.partTwo(cmds)
+    assertEquals(res, 900)
+  
+  @Test def partTwo(): Unit =
+    val res = Day2.partTwo()
+    assertEquals(res, 1848454425)
