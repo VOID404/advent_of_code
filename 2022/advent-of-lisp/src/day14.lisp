009de5a590e455e211883afd8a4d374f5e419406
VOID404 <wojciech.nawa@gmail.com>
2023-02-07 12:51:03 +0100

    Refactor day14 [WIP]

--- 2022/advent-of-lisp/src/day14.lisp
+++ 2022/advent-of-lisp/src/day14.lisp
@@ -7,182 +7,127 @@
 
 (defvar *max-y* 0)
 
+(defparameter *sample* "498,4 -> 498,6 -> 496,6
+503,4 -> 502,4 -> 502,9 -> 494,9")
+
 (defstruct point
-  (x 0 :type integer)
-  (y 0 :type integer))
+  (x 0 :type fixnum)
+  (y 0 :type fixnum))
 
-(defstruct bottom
-  (a nil :type point)
-  (b nil :type point))
+(defstruct bounds
+  (x-min nil :type fixnum)
+  (x-max nil :type fixnum)
+  (y-min nil :type fixnum)
+  (y-max nil :type fixnum))
 
-(defun point= (a b)
-  (declare (type point a b))
-  (and (= (point-x a)
-          (point-x b))
-       (= (point-y a)
-          (point-y b))))
+(defun bounds-merge (a b)
+  "Returns new bounds, that fit both arguments"
+  (declare (type bounds a b))
+  (let ((x-max (max (bounds-x-max a)
+                    (bounds-x-max b)))
+        (x-min (min (bounds-x-min a)
+                    (bounds-x-min b)))
+        (y-max (max (bounds-y-max a)
+                    (bounds-y-max b)))
+        (y-min (min (bounds-y-min a)
+                    (bounds-y-min b))))
+    (make-bounds :x-min x-min :x-max x-max
+                 :y-min y-min :y-max y-max)))
+(defun bounds-width (bound)
+  (declare (type bounds bound))
+  (the fixnum (- (bounds-x-max bound)
+                 (bounds-x-min bound)
+                 -1)))
 
-(defun point-add (a b)
-  (declare (type point a b))
-  (make-point :x (+ (point-x a)
-                    (point-x b))
-              :y (+ (point-y a)
-                    (point-y b))))
-
-;; https://stackoverflow.com/a/11908158
-(defun on-line-p (p a b &optional infinite)
-  "Check if p is on line between a and b"
-  (declare (type point p a b))
-  (let* ((ax (point-x a))
-         (ay (point-y a))
-         (bx (point-x b))
-         (by (point-y b))
-         (x (point-x p))
-         (y (point-y p))
-         (dxc (- x ax))
-         (dyc (- y ay))
-         (dxl (- bx ax))
-         (dyl (- by ay))
-         (cross (- (* dxc dyl) (* dyc dxl))))
-    (when (= cross 0)
-      (or infinite
-        (if (>= (abs dxl) (abs dyl))
-            (if (< 0 dxl)
-                (and (<= ax x)
-                     (<= x bx))
-                (and (<= bx x)
-                     (<= x ax)))
-            (if (< 0 dyl)
-                (and (<= ay y)
-                     (<= y by))
-                (and (<= by y)
-                     (<= y ay))))))))
-
-(defun poly-collides (pnt arr)
-  "Takes a point and list of points, check if point lies on any line"
-  (declare (type point pnt))
-  (declare (type list arr))
-  (iter
-    (for a in arr)
-    (for b in (cdr arr))
-    (thereis (on-line-p pnt a b))))
-
-(defun collides (pnt obstacles)
-  "Given a point and list of poly lines, returns true if point on any line"
-  (iter
-    (for o in obstacles)
-    (finding o such-that (typecase o
-                           (list (poly-collides pnt o))
-                           (point (point= pnt o))
-                           (bottom (on-line-p pnt (bottom-a o) (bottom-b o) t))))))
-
-(defparameter +down+ (make-point :y 1))
-(defparameter +up+ (make-point :y -1))
-(defparameter +left+ (make-point :x -1))
-(defparameter +right+ (make-point :x 1))
-(defparameter +generator+ (make-point :x 500))
-
-(defparameter *obstacles* (list ;; 498,4 -> 498,6 -> 496,6
-                           (list (make-point :x 498 :y 4)
-                                 (make-point :x 498 :y 6)
-                                 (make-point :x 496 :y 6))
-                           (list (make-point :x 503 :y 4)
-                                 (make-point :x 502 :y 4)
-                                 (make-point :x 502 :y 9)
-                                 (make-point :x 494 :y 9))))
-
-
-;; 12 x 23)
-(defun render (obstacles height width)
-  (flet ((to-char (o) (etypecase o
-                        (point #\o)
-                        (list #\#))))
-    (iter
-      (with canvas = (make-array (list height width)
-                                 :initial-element #\.))
-      (for i from 0 to (* height width))
-      (for x = (mod i width))
-      (for y = (floor i width))
-      (for c = (collides (make-point :x (+ 490 x)
-                                     :y y)
-                         obstacles))
-      (if c
-          (setf (aref canvas y x) (to-char c)))
-      (finally (dotimes (y height)
-                 (dotimes (x width)
-                   (princ (aref canvas y x)))
-                 (fresh-line))))))
-
-(defun drop (point obstacles &optional (max-y *max-y*))
-  (if (collides point obstacles)
-      nil
-      (iter
-        (for p initially point then next)
-        (for next = (point-add p +down+))
-
-        (until (or (= max-y (point-y p))
-                   (and (point= p point)
-                        (not (first-iteration-p)))))
-
-        (when (collides next obstacles)
-          (let ((left (point-add next +left+))
-                (right (point-add next +right+)))
-            (cond ((not (collides left obstacles)) (setf next left))
-                  ((not (collides right obstacles)) (setf next right))
-                  (t (return p))))))))
-
-(defun simulate (obstacles)
-  (iter
-    (for i from 0)
-    (for d = (drop +generator+ obstacles))
-    (while d)
-    (push d obstacles)
-    (counting d)))
+(defun bounds-height (bound)
+  (declare (type bounds bound))
+  (the fixnum (- (bounds-y-max bound)
+                 (bounds-y-min bound)
+                 -1)))
 
 (defun read-points (&optional (stream *standard-input*))
+  "Returns list of points and bounds"
   (let ((*readtable* (copy-readtable)))
     (set-macro-character #\, (lambda (stream char)
                                (declare (ignore stream char))
                                (values)))
     (iter
       (for s = (read stream nil :eof))
-      (cond
-        ((eq s '->)
-         (collect (make-point :x (car buf)
-                              :y (cadr buf)) into out)
-         (maximizing (cadr buf) into maxy)
-         (setf buf nil))
-        ((eq s :eof)
-         (collect (make-point :x (car buf)
-                              :y (cadr buf)) into out)
-         (maximizing (cadr buf) into maxy)
-         (finish))
-        (t (collect s into buf)))
-      (finally (return (values out maxy))))))
-
-
-(defparameter *sample* "498,4 -> 498,6 -> 496,6
-503,4 -> 502,4 -> 502,9 -> 494,9")
+      (if (or (eq s '->)
+              (eq s :eof))
+          (let* ((x (car buf))
+                 (y (cadr buf))
+                 (point (make-point :x x :y y)))
+            (collect point into points)
+            (maximizing x into max-x)
+            (maximizing y into max-y)
+            (minimizing x into min-x)
+            (minimizing y into min-y)
+            (setf buf nil))
+          (collect s into buf))
+      (when (eq s :eof)
+        (return (values points
+                        (make-bounds :x-min min-x :x-max max-x
+                                     :y-min min-y :y-max max-y)))))))
 
 (defun read-obstacles (&optional (stream *standard-input*))
+  "Returns list of lists of points and bounds"
   (iter
+    (with bounds = 0)
     (for l = (read-line stream nil :eof))
     (until (eq l :eof))
     (unless (string= l "")
-      (multiple-value-bind (points max-y) (with-input-from-string (s l) (read-points s))
-        (collect points into pnts)
-        (maximize max-y into maxy)))
-    (finally (return (values pnts maxy)))))
+      (multiple-value-bind (p b) (with-input-from-string (s l) (read-points s))
+        (collect p into points)
+        (if (first-time-p)
+            (setf bounds b)
+            (setf bounds (bounds-merge bounds b)))))
+    (finally (return (values points bounds)))))
+
+(defun obstacle->lines (obstacle)
+  (iter
+    (for a in obstacle)
+    (for b in (cdr obstacle))
+    (collecting (cons a b))))
+
+(defun point-distance (a b)
+  (+ (abs (- (point-x a) (point-x b)))
+     (abs (- (point-y a) (point-y b)))))
+
+(defun obstacle-collides (p obstacles)
+  "Check if point collides with any obstacle"
+  (iter
+    (for o in obstacles)
+    (thereis
+     (iter
+       (for l in (obstacle->lines o))
+       (for a = (car l))
+       (for b = (cdr l))
+       (for dist0 = (point-distance a b))
+       (for dist1 = (point-distance p a))
+       (for dist2 = (point-distance p b))
+       (thereis (= dist0 (+ dist1 dist2)))))))
+
+(defun obstacles->map (obstacles bounds)
+  (declare (type bounds bounds))
+  (declare (type list obstacles))
+  (let* ((min-x (bounds-x-min bounds))
+         (width (bounds-width bounds))
+         (height (1+ (bounds-y-max bounds)))
+         (len (* height width))
+         (map (make-array len ;:element-type 'boolean
+                              :initial-element 0)))
+    (iter
+      (for i index-of-vector map)
+      (for x = (+ min-x (mod i width)))
+      (for y = (floor i width))
+      (for p = (make-point :x x :y y))
+      (when (obstacle-collides p obstacles)
+        (setf (aref map i) 1)))
+    (make-array (list height width)
+                :element-type 'boolean
+                :displaced-to map)))
 
 (defun basic (&optional (stream *standard-input*))
-  (multiple-value-bind (obstacles *max-y*) (read-obstacles stream)
-    (incf *max-y* 2)
-    (simulate obstacles)))
-
-(defun bonus (&optional (stream *standard-input*))
-  (multiple-value-bind (obstacles *max-y*) (read-obstacles stream)
-    (incf *max-y* 2)
-    (push (make-bottom :a (make-point :x 0 :y *max-y*)
-                       :b (make-point :x 1 :y *max-y*))
-          obstacles)
-    (simulate obstacles)))
+  (multiple-value-bind (obstacles bounds) (read-obstacles stream)
+    (obstacles->map obstacles bounds)))
